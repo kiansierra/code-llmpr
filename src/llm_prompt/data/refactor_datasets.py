8d9d1e3903db60a7497de06542b50d51dd74ddf1
kiansierra <kiansierra90@gmail.com>
2024-03-10 19:10:03 +0100

    added datasets configs

--- src/llm_prompt/data/refactor_datasets.py
+++ src/llm_prompt/data/refactor_datasets.py
@@ -3,12 +3,26 @@ from datasets import load_dataset, Dataset, concatenate_datasets
 
 NEW_KEY_COLUMN = "original_text"
 
-__all__ = ["dataset_preprocess"]
+__all__ = ["dataset_preprocess", "REWRITE_TEMPLATES"]
 
 def dataset_preprocess(dataset_name: str, key_column: str ,subsets:Optional[List[str]] = None) -> Dataset:
     subsets = subsets or ["train"]
-    dataset = load_dataset(dataset_name)
-    dataset = dataset.rename_column(key_column, NEW_KEY_COLUMN)
-    dataset = dataset.select_columns([NEW_KEY_COLUMN])
-    dataset = concatenate_datasets([dataset[subset] for subset in subsets])
+    datasets = load_dataset(dataset_name)
+    datasets = datasets.rename_column(key_column, NEW_KEY_COLUMN)
+    datasets = datasets.select_columns([NEW_KEY_COLUMN])
+    dataset_list = []
+    for subset in subsets:
+        dataset = datasets[subset]
+        dataset = dataset.add_column("source", [dataset_name] * len(dataset))
+        dataset = dataset.add_column("split", [subset] * len(dataset))
+        dataset_list.append(dataset)
+    dataset = concatenate_datasets(dataset_list)
     return dataset
+
+
+REWRITE_TEMPLATES = [
+    "Rewrite in the style of a sea shanty. {}",
+    "Improve the text. {}",
+    "Rewrite this essay but do it using the writing style of Dr. Seuss. {}",
+    "Rewrite this essay but do it using the writing style of William Shakespeare. {}",
+    "Rewrite this essay but do it using the writing style of Tupac Shakur. {}",]
