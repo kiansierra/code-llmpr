65a61f91c27b800560612fbf55b8bee14a69c9fb
kiansierra <kiansierra90@gmail.com>
2024-04-05 23:14:17 +0200

    adding prompt cluster and source

--- scripts/03_add_templates.py
+++ scripts/03_add_templates.py
@@ -12,18 +12,23 @@ SPLITS = ["train", "validation", "test"]
 
 NUM_PROMPTS_PER_TEXT = 4
 INPUT_DATA_DIR = os.environ.get("INPUT_DATA_DIR", "../input")
-INPUT_DATASET_NAME = "raw_texts"
+INPUT_TEXTS_DATASET_NAME = "raw_texts"
+INPUT_PROMPTS_DATASET_NAME = "prompts"
+
 OUTPUT_DATASET_NAME = "templates"
 
 
 def main() -> None:
     run = wandb.init(job_type="add_templates")
-    artifact = run.use_artifact(f"{INPUT_DATASET_NAME}:latest")
-    datadir = artifact.download(f"./artifacts/{INPUT_DATASET_NAME}")
+    artifact = run.use_artifact(f"{INPUT_TEXTS_DATASET_NAME}:latest")
+    datadir = artifact.download(f"./artifacts/{INPUT_TEXTS_DATASET_NAME}")
     raw_dataset_dict = load_from_disk(datadir)
+    artifact = run.use_artifact(f"{INPUT_PROMPTS_DATASET_NAME}:latest")
+    datadir = artifact.download(f"./artifacts/{INPUT_PROMPTS_DATASET_NAME}")
+    prompts_df = pd.read_parquet(f"./artifacts/{INPUT_PROMPTS_DATASET_NAME}/prompts.parquet")
     dataset_templates = []
-    prompts_df = pd.read_csv("prompts.csv")
-    all_rewrite_prompts = REWRITE_PROMPTS + prompts_df["rewrite_prompt"].tolist()
+    all_rewrite_prompts = prompts_df["rewrite_prompt"].tolist()
+    prompts_df = prompts_df.rename(columns={"source": "prompt_source", "cluster": "prompt_cluster"})
     logger.info(f"Number of rewrite prompts: {len(all_rewrite_prompts)}")
     for name, dataset in raw_dataset_dict.items():
         logger.info(f"Adding prompts to {name}")
@@ -31,6 +36,8 @@ def main() -> None:
         dataset = dataset.add_column("rewrite_prompt", selected_prompts.tolist())
         df = dataset.to_pandas()
         df = df.explode("rewrite_prompt")
+        logger.info(f"Merging prompts metadata to {name}")
+        df = df.merge(prompts_df[['rewrite_prompt','prompt_source', 'prompt_cluster']], on='rewrite_prompt')
         logger.info(f"Number of examples in {name}: {len(df)}")
         dataset_templates.append(Dataset.from_pandas(df))
     dataset = concatenate_datasets(dataset_templates)
